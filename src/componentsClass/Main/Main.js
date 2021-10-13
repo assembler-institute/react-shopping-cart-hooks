7405edda5cd6b14eeb35c668fc5bdcf5121dcb00
Paola3stefania <paolaestefania.decampos@gmail.com>
2021-10-13 18:41:53 +0200

    --old trash code erased

--- src/componentsClass/Main/Main.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Main({ children, ...props }) {
-  return <main {...props}>{children}</main>;
-}
-
-export default Main;
--- src/componentsClass/SVGIcons/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ThumbUp } from "./ThumbUp";
-export { default as ThumbDown } from "./ThumbDown";
-export { default as SavedIcon } from "./SavedIcon";
-export { default as UnsavedIcon } from "./UnsavedIcon";
